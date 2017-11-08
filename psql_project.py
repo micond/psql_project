06fa6f48ab6d422f5ea369a01237da3ec1ac1b57
Michal Ondrasek <michal.ondrasek@gmail.com>
2017-11-08 19:13:23 +0100

    Fixing queries + output.txt file

--- psql_project.py
+++ psql_project.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import psycopg2
 
 
@@ -5,9 +6,9 @@ def get_most_popular_articles():
     """Returns the most popular three articles of all time"""
     db = psycopg2.connect(dbname="news")
     c = db.cursor()
-    c.execute("""select articles.title, count(log.path) from log join articles
-                 ON articles.slug = trim(both from log.path, '/article/')
-                 GROUP BY articles.title limit 3""")
+    c.execute("""select title, count(*) as views from log join articles
+                 on log.path = concat('/article/', articles.slug) group by
+                 title order by views desc limit 3;""")
 
     print("1. What are the most popular three articles of all time?")
     for row in c:
@@ -25,9 +26,9 @@ def get_most_popular_authors():
     c = db.cursor()
     c.execute("""select authors.name, COALESCE(t.count,'0') from authors left
                  join (select articles.author, count(log.path) from log join
-                 articles ON articles.slug = trim(both from log.path,
-                 '/article/') GROUP BY articles.author ORDER BY count DESC)
-                 AS t ON authors.id = t.author""")
+                 articles ON concat('/article/', articles.slug) = log.path
+                 GROUP BY articles.author ORDER BY count DESC)
+                 AS t ON authors.id = t.author;""")
 
     print("2. Who are the most popular article authors of all time?")
     for row in c:
@@ -43,16 +44,11 @@ def get_lead_errors():
     """Returns day with more than 1% of requests lead to error"""
     db = psycopg2.connect(dbname="news")
     c = db.cursor()
-    c.execute("""select date, round((cnt/((select count(status) from (select
-                 date(time),status from log where status like '%4%' OR status
-                 like '%5%') as x)/100)::float)::numeric,2)|| '% errors' as
-                 percentage from (select to_char(date, 'FMMonth DD,YYYY') as
-                 date, count(status) as CNT from (select date(time),status
-                 from log where status like '%4%' OR status like '%5%') as t
-                 GROUP BY date order by CNT DESC) as y
-                 where cnt > ((select count(status) from (select date(time),
-                 status from log where status like '%4%' OR status like '%5%')
-                 as x)/100);""")
+    c.execute("""select to_char(date,'Mon DD, YYYY'), round((((err/total)*100)
+                 ::float)::numeric,2)|| '% errors' as percentage from
+                 (select time::date as date, count(*) as total,
+                 sum((status != '200 OK')::int)::float as err from log group
+                 by date) as foo where ((err/total)*100) > 1;""")
 
     print("3. On which days did more than 1% of requests lead to errors?")
     for row in c:
